b56f1bd7c938dd28cd25fc64bb65c16a243948d1
thinh <caodangthinh1@gmail.com>
2024-03-11 14:01:10 +0700

    fix button name

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
                         <label><b>Mật khẩu</b></label>
                         <input type="password" name="txtPassword" class="form-control" required>
                     </div>
-                    <button type="submit" class="btn btn-primary mt-3" name="btnLogin">Login</button>
+                    <button type="submit" class="btn btn-primary mt-3" name="btnLogin">Đăng nhập</button>
                 </form>
             </div>
         </div>
